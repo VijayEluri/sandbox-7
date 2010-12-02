ece26d98b42ffcfe2e57bebaf6cd86f688fc725b
Junichi Kato <j5ik2o@gmail.com>
2010-12-02 11:18:17 +0900

    Failed commit: Default

---
+++ dxo-dsl/src/test/scala/jp/tricreo/dxodsl/DxoTest.scala
@@ -0,0 +1,15 @@
+package jp.tricreo.dxodsl
+
+import org.scalatest.FunSuite
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: junichi
+ * Date: 2010/12/02
+ * Time: 11:17:15
+ * To change this template use File | Settings | File Templates.
+ */
+
+class DxoTest extends FunSuite{
+  
+}
